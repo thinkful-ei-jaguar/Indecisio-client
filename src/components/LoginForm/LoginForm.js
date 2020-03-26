2eace550dffd57fca66305bf8b8a23a8777d5dfa
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-03-26 14:06:41 -0400

    got RegistrationForm to communicate with server, now working on login

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,22 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ValidationError from '../ValidationError/ValidationError';
-//import AuthApiService from '../../services/auth-api-service';
+import UserContext from '../contexts/UserContext'
+import AuthApiService from '../services/auth-api-service';
 
 class LoginForm extends Component {
+    static contextType = UserContext
+
     state = { error: null }
+
+    firstInput = React.createRef()
+
+    componentDidMount() {
+        this.firstInput.current.focus()
+    }
+
+    handleLogin = (e) => {
+        e.preventDefault()
+        const {username, password} = e.target
+        
+    }
 
     render() {
         const { error } = this.state
         return (
             <section id='form-wrapper'>
-            <form className="login-form activity-form">
+            <form className="login-form activity-form" onSubmit={e => this.handleLogin(e)}>
                 <h2>Login</h2>
 
                 <label className='form-input-label' htmlFor='username'>Username</label>
-                    <input className='login-form-username-input activity-form-text-input' name='username' placeholder='Username' type='text' required/>
+                    <input className='login-form-username-input activity-form-text-input' ref={this.firstInput} name='username' placeholder='Username' type='text' required/>
                 <label className='form-input-label' htmlFor='password'>Password</label>
-                    <input className='login-form-password-input activity-form-text-input' name='password' placeholder='Password' type='text' required/>
+                    <input className='login-form-password-input activity-form-text-input' name='password' placeholder='Password' type='password' required/>
                     {error && (<ValidationError message={error} />)}
                 <button className='button-primary' type='submit'>Submit</button>
                 <Link to='/' className='login-redirect-link'>
