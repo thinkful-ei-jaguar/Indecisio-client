961558f6a82af177390f9f596b3b09d3858fc385
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-04-03 14:53:19 -0400

    sending up basic expansion on user top ten

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -1,11 +1,27 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 import ProfileService from '../services/profile-service';
+import UserContext from '../contexts/UserContext';
 import '../UserProfile/UserProfile.css';
 
 export default class UserProfile extends Component {
+    static contextType = UserContext
+
     state = {
         error: null,
-        topUserActivities: []
+        topUserActivities: [],
+        expanded: false
+    }
+
+    handleOpen = () => {
+        this.setState({
+            expanded: true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            expanded: false
+        })
     }
 
     componentDidMount() {
@@ -15,19 +31,28 @@ export default class UserProfile extends Component {
                     topUserActivities: res
                 })
             })
+            .catch(error => {
+                this.setState({
+                    error: error.message
+                })
+            })
     }
 
     render() {
-        const {error, topUserActivities} = this.state
+        const {user} = this.context
+        const {error, topUserActivities, expanded} = this.state
         return(
         <section id="form-wrapper">
-            <h2>Your Top Activities</h2>
+            <h2>{user.name}'s Top Activities</h2>
             {error && <p>{error}</p>}
             <ol>
                 {topUserActivities.map(activity => {
-                    return <li>
-                                <h3>{activity.name}</h3>
-                                {/* <p>You've completed this activity {activity.accepted_count} times!</p> */}
+                    return <li key={activity.id}>
+                                {expanded 
+                                    ?<><h3>{activity.name}</h3><p>{activity.description}</p>
+                                        <p>You've completed this activity {activity.accepted_count} times!</p>
+                                        <p onClick={e => this.handleClose()}>close meh!</p></>
+                                    :<><h3>{activity.name}</h3><p onClick={e => this.handleOpen()}>click meh!</p></>}
                             </li>
                 })}
             </ol>
